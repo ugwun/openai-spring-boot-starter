4eea1fe44eb458e30ce8b1f1f1a4c88a0cd87ab5
c.sadovsky@telekom.com <c.sadovsky@telekom.com>
2023-06-19 09:05:22 +0200

    Initial commit

---
+++ src/test/java/com/aiisforhumans/springbootstarteropenai/SpringbootstarteropenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiisforhumans.springbootstarteropenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstarteropenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
